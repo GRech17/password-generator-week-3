49d3f28fed464ea08e6407788dfc57f55f623378
GIR <gireches@gmail.com>
2020-12-30 21:44:51 -0800

    case check/password shows

--- script.js
+++ script.js
@@ -1,39 +1,94 @@
-// Assignment code here
+// Assignment code
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
-let uppercase = "ABCDEFGHIJKLMNOP";
+let Ucase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let number = "0123456789";
 let shiftCharacter = "!#$%&'()*+,-./:;<=>?@[]\^_`{}~|";
-let Length;
+let wordLength;
 let caseCheck; 
 let numberCheck;
 let shiftCharacterCheck;
 
 // Determines length of password
 function length () {
-  Length = prompt("Choose a number between 8-128 for your password length");
+  wordLength = prompt("Choose a number between 8-128 for your password length");
 
-  if (Length<8){
-    alert("Password must be between 8-128 characters");
-    length(); 
-  } else if (Length>128) {
-    alert("Password must be between 8-128 characters");
-    length(); 
-  } else if (isNaN(Length)){
-    alert("Password must be between 8-128 characters");
-    length();
+    if (wordLength<8){
+      alert("Password must be between 8-128 characters");
+      length(); 
+  } 
+  
+    else if (wordLength>128) {
+      alert("Password must be between 8-128 characters");
+      length(); 
+  } 
+  
+    else if (isNaN(wordLength)){
+      alert("Password must be between 8-128 characters");
+      length();
     
-    return Length;
+      return wordLength;
   }
 }
 
+// Determines if there are uppercase letters in the password
+function casing() {
+  caseCheck = prompt("Include uppercase letters in your password? \n ('yes' or 'no')");
+
+    if (caseCheck === "y" || caseCheck === "yes"){
+      caseCheck = true;
+      return caseCheck;
+    }
+
+    else if (caseCheck === "n" || caseCheck === "no"){
+      caseCheck = false;
+      return caseCheck;
+    }
+
+    else if (caseCheck === null || caseCheck === " "){
+      alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
+      casing ();
+    }
+
+    else {
+      alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
+      casing ();
+    }
+    
+    return caseCheck;
+  }
+
+// Determines if there are numbers in the password
+
+// Determines if there are shiftCharacter's in the password
+
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Generates a password 
 function generatePassword(){
   length();
-  console.log(Length);
+  console.log(wordLength);
+  casing();
+  console.log(caseCheck);
+
+let char = lowercase;
+let Password = "";
+if (caseCheck){
+  char += Ucase;
 }
+
+else {
+  char += lowercase;
+}
+
+
+for (let i = 0; i < wordLength; i++){
+  Password += char.charAt(Math.floor(Math.random() * char.length));
+}
+return Password;
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
