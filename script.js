64b0fb48f036c761c2d969f982dce441c29340b3
GIR <gireches@gmail.com>
2021-01-03 15:35:05 -0800

    finished product

--- script.js
+++ script.js
@@ -2,32 +2,32 @@
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
 let Ucase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let number = "0123456789";
-let shiftCharacter = "!#$%&'()*+,-./:;<=>?@[]\^_`{}~|";
+let shiftCharacter = "!#$%&'()*+,-./:;<=>?@[]^_`{}~|";
 let wordLength;
 let caseCheck; 
 let numberCheck;
 let shiftCharacterCheck;
 
 // Determines length of password
-function length () {
+function length() {
   wordLength = prompt("Choose a number between 8-128 for your password length");
 
     if (wordLength<8){
-      alert("Password must be between 8-128 characters");
+      alert("Only numbers between 8-128 are acceptable");
       length(); 
   } 
   
     else if (wordLength>128) {
-      alert("Password must be between 8-128 characters");
+      alert("Only numbers between 8-128 are acceptable");
       length(); 
   } 
   
     else if (isNaN(wordLength)){
-      alert("Password must be between 8-128 characters");
+      alert("Only numbers between 8-128 are acceptable");
       length();
+  }  
     
-      return wordLength;
-  }
+  return wordLength;
 }
 
 // Determines if there are uppercase letters in the password
@@ -46,45 +46,124 @@ function casing() {
 
     else if (caseCheck === null || caseCheck === " "){
       alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
-      casing ();
+      casing();
     }
 
     else {
       alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
-      casing ();
+      casing();
     }
     
     return caseCheck;
   }
 
 // Determines if there are numbers in the password
+function numberFunc() {
+  numberCheck = prompt("Include numbers in your password? \n ('yes' or 'no')");
+
+  if (numberCheck === "y" || numberCheck === "yes"){
+    numberCheck = true;
+    return numberCheck;
+  }
+
+  else if (numberCheck === "n" || numberCheck === "no"){
+    numberCheck = false;
+    return numberCheck;
+  }
+
+  else if (numberCheck === null || numberCheck === ""){
+    alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
+    numberFunc();
+  }
+
+  else {
+    alert("Only 'y' 'yes' or 'n' 'no' is acceptable")
+    numberFunc();
+  }
+
+  return numberCheck;
+}
 
 // Determines if there are shiftCharacter's in the password
+function shiftChar() {
+  shiftCharacterCheck = prompt("Include special shift characters in your password? \n ('yes' or 'no')");
+
+  if (shiftCharacterCheck === "y" || shiftCharacterCheck === "yes"){
+    shiftCharacterCheck = true; 
+    return shiftCharacterCheck;
+  }
+
+  else if (shiftCharacterCheck === "n" || shiftCharacterCheck === "no"){
+    shiftCharacterCheck = false;
+    return shiftCharacterCheck;
+  }
+
+  else if (shiftCharacterCheck === null || shiftCharacterCheck === ""){
+    alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
+    shiftChar();
+  }
+
+  else {
+    alert("Only 'y' 'yes' or 'n' 'no' is acceptable");
+    shiftChar();
+  }
+
+  return shiftCharacterCheck;
+}
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
-// Generates a password 
+// Generates a password with conditional statements 
 function generatePassword(){
   length();
   console.log(wordLength);
   casing();
   console.log(caseCheck);
+  numberFunc();
+  console.log(numberCheck);
+  shiftChar();
+  console.log(shiftCharacterCheck);
 
 let char = lowercase;
 let Password = "";
-if (caseCheck){
+
+if (caseCheck && numberCheck && shiftCharacterCheck){
+  char += Ucase + number + shiftCharacter;
+}
+
+else if (caseCheck && numberCheck){
+  char += Ucase + number;
+}
+
+else if (numberCheck && shiftCharacterCheck){
+  char += number + shiftCharacter;
+}
+
+else if (caseCheck && shiftCharacterCheck){
+  char += Ucase + shiftCharacter;
+}
+
+else if (caseCheck){
   char += Ucase;
 }
 
-else {
-  char += lowercase;
+else if (numberCheck){
+  char += number;
 }
 
+else if (shiftCharacterCheck){
+  char += shiftCharacter;
+}
+
+else {
+  char === lowercase;
+}
 
 for (let i = 0; i < wordLength; i++){
   Password += char.charAt(Math.floor(Math.random() * char.length));
 }
+
 return Password;
 
 }
